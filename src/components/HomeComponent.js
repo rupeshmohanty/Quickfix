7605d30bf43f6269f97bf26d2fc74cab63d272e9
Rupesh Chandra Mohanty <43889891+rupeshmohanty@users.noreply.github.com>
2021-03-29 17:10:29 +0530

    Home component code changed

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
 // importing semantic ui components!
-import { Card, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
+
+// importing css
+import './css/BlogComponent.css';
 
 // importing components
 import NavbarComponent from './NavbarComponent';
@@ -10,28 +13,44 @@ class HomeComponent extends Component{
     render() {
         return(
             <div className = "home">
-                <NavbarComponent/><br/><br/><br/>
+                <NavbarComponent/>
                 <Container>
-                    <div className = "row">
-                        <div className = "col-md-4">
-                            <Card>
-                                <Card.Content header = 'Lorem Ipsum'></Card.Content>
-                                <Card.Content description = 'Some temporary text'></Card.Content>
-                            </Card>
+                    <section className = "details-card">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <div className="card-content">
+                                        <div className="card-desc">
+                                            <h3>Heading</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                                                voluptas totam</p>
+                                                <a href="#" className="btn-card">Read</a>   
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card-content">
+                                        <div className="card-desc">
+                                            <h3>Heading2</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                                                voluptas totam</p>
+                                                <a href="#" className="btn-card">Read</a>   
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card-content">
+                                        <div className="card-desc">
+                                            <h3>Heading3</h3>
+                                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                                                voluptas totam</p>
+                                                <a href="#" className="btn-card">Read</a>   
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className = "col-md-4">
-                            <Card>
-                                <Card.Content header = 'Lorem Ipsum'></Card.Content>
-                                <Card.Content description = 'Some temporary text'></Card.Content>
-                            </Card>
-                        </div>
-                        <div className = "col-md-4">
-                            <Card>
-                                <Card.Content header = 'Lorem Ipsum'></Card.Content>
-                                <Card.Content description = 'Some temporary text'></Card.Content>
-                            </Card>
-                        </div>
-                    </div>
+                    </section>
                 </Container>
             </div>
         )
